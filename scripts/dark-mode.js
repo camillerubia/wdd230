a7fd0b4374b89acc8ab8da2ffb3f4e7ca03b46db
Camille Rubia <va.camillerubia@gmail.com>
2023-09-21 23:13:12 +0800

    Home page enhancement dry run build

---
+++ scripts/dark-mode.js
@@ -0,0 +1,14 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("✩☾")) {
+        main.style.background = "#081C15";
+        main.style.color = "#081C15";
+        modeButton.textContent = "☀";
+    } else {
+        main.style.background = "#D8F3DC";
+        main.style.color = "#000";
+        modeButton.textContent = "✩☾";
+    }
+});
