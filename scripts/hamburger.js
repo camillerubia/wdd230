c6ca6098c35152d097f06b79e3a771418857f82c
Camille Rubia <va.camillerubia@gmail.com>
2023-09-23 13:04:11 +0800

    Finished chamber homepage dry run build

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,9 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const navWrapper = document.querySelector('nav');
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
+    navWrapper.classList.toggle('open');
 })
