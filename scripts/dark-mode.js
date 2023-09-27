777bbaf68d1ff0ffd7f2b3eb7b88a2cda37b12af
Camille Rubia <va.camillerubia@gmail.com>
2023-09-27 17:03:58 +0800

    Finished homepage responsive image activity

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,14 +1,31 @@
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+const card = document.querySelector(".card");
+const h3 = document.querySelector("h3");
+const hero = document.querySelector(".hero");
+const title = document.querySelector('.page-title');
+const link = card.querySelectorAll("a");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("✩")) {
-        main.style.background = "#081C15";
-        main.style.color = "#081C15";
+        h3.style.color = "#081C15";
         modeButton.textContent = "☀";
+
+        card.style.background = "#081C15";
+        card.style.color = "#B7E4C7";
+        
+        link.forEach(link => {
+            link.style.color = "#B7E4C7";
+        });
+        
+        
     } else {
-        main.style.background = "#D8F3DC";
-        main.style.color = "#000";
         modeButton.textContent = "✩";
+
+        card.style.background = "#fff";
+        card.style.color = "#000";
+
+        link.forEach(link => {
+            link.style.color = "#081C15";
+        });
     }
 });
