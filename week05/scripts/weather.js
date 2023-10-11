e8726126b4686c93c94c535d007945e6c55a7ca2
Camille Rubia <va.camillerubia@gmail.com>
2023-10-11 23:32:32 +0800

    Finished W05 Prophets activity

---
+++ week05/scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75248387068769}&lon={6.637776763463326}&appid={ce849aac9e4ca7eb729446e0c2683116}';
