5db6f18c91005479aaf63700199bd6a0519e1fac
Camille Rubia <va.camillerubia@gmail.com>
2023-10-12 16:56:15 +0800

    Finished W05 Weather activity

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,5 +1,34 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('#figcaption');
+const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75248387068769}&lon={6.637776763463326}&appid={ce849aac9e4ca7eb729446e0c2683116}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=ce849aac9e4ca7eb729446e0c2683116';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            console.log('OK');
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+        console.log('ERROR');
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'Current weather in Trier, Germany');
+    captionDesc.textContent = `${desc}`;
+}
