836013e4c168c3430d470113554009a1e8b5bffc
Camille Rubia <va.camillerubia@gmail.com>
2023-10-13 10:31:46 +0800

    Finished Homepage Weather and populating links in the Learning Activities

--- scripts/links.js
+++ scripts/links.js
@@ -6,25 +6,28 @@ async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     // console.log(data);
-    displayLinks(data);
+    displayLinks(data.weeks);
 }
 
 const displayLinks = (weeks) => {
     let activityList = document.createElement('ul');
-    
 
     weeks.forEach((week) => {
-        console.log(weeks);
         let list = document.createElement('li');
         let weekNum = document.createElement('span');
-
-        weekNum.textContent = `${week.week}`;
+        weekNum.textContent = `${week.week}: `;
         list.appendChild(weekNum);
-                
-        weeks.links.forEach((link) => {
+
+        week.links.forEach((link,index) => {
             let activityLink = document.createElement('a');
-            activityLink.textContent = `${links.title}`;
-            activityLink.setAttribute('href', `${links.url}`);
+            
+            if (index !== week.links.length - 1) {
+                activityLink.textContent = ` ${link.title} |`;
+            } else {
+                activityLink.textContent = ` ${link.title}`;
+            }
+
+            activityLink.setAttribute('href', `${link.url}`);
             list.appendChild(activityLink);
         });
 
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,5 +1,6 @@
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
+const weatherHolder = document.querySelector('#weather-icon');
+const weatherIcon = document.createElement('img');
 const captionDesc = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=15.29&lon=120.03&appid=ce849aac9e4ca7eb729446e0c2683116';
@@ -26,6 +27,7 @@ function displayResults(data) {
     currentTemp.innerHTML = `${(data.main.temp - 273.15).toFixed(2)}&deg;C - `;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
+    weatherHolder.appendChild(weatherIcon);
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', 'Current weather in Botolan, Zambales');
     captionDesc.textContent = `${desc}`;
