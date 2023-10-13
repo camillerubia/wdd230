277f3278cde3521105ad6055bc4f9b7f636b0db9
Camille Rubia <va.camillerubia@gmail.com>
2023-10-13 20:56:21 +0800

    testing JSON file

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,9 +1,38 @@
 const gridDisplay = document.querySelector('.grid-display');
+const memberLink = 'https://camillerubia.github.io/wdd230/chamber/data/members.json';
 
 async function getMemberData() {
-    const response = await fetch(members.json);
+    const response = await fetch(memberLink);
     const data = await response.json();
-    console.table(data.members);
+    displayMembers(data.members);
+};
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let logo = document.createElement('img');
+        let name = document.createElement('p');
+        let address = document.createElement('p');
+        let number = document.createElement('p');
+        let website = document.createElement('a');
+
+        name.textContent = `${member.name}`;
+        address.textContent = `${member.address}`;
+        number.textContent = `${member.phoneNumber}`;
+        website.textContent = `${member.url}`;
+
+        logo.setAttribute('src', member.logo);
+        logo.setAttribute('alt', `Company logo of ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '250');
+        logo.setAttribute('height', '250');
+
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(number);
+        gridDisplay.appendChild(card);
+    });
 }
 
-getMembersData();
+getMemberData();
