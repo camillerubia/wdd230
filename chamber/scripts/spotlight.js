fd3d38cc4a5de8815993831a4ff5fe605333a05e
Camille Rubia <va.camillerubia@gmail.com>
2023-10-24 22:44:15 +0800

    Changed the JSON file list name

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -4,11 +4,27 @@ async function getMemberData() {
     const response = await fetch(spotlightInfo);
     const data = await response.json();
     console.log(data);
-    // displayRandomAds(data.elite-members);
+    displayRandomAds(data.elite-members);
 }
 
 function displayRandomAds(members) {
-    
+    members.forEach((member => {
+        console.log(member[randomizer()]);
+    }))
+}
+
+function randomizer() {
+    let randomNumber;
+
+    while (true) {
+        randomNumber = Math.floor(Math.random() * 10);
+
+        if (randomNumber <= 3) {
+            break;
+        }
+    }
+
+    return randomNumber;
 }
 
 getMemberData();
